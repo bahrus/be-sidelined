f604298cc9fa32d53132cf4b77361db69da9aedd
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-08-30 21:04:13 -0400

    Initial commit

--- types.d.ts
+++ types.d.ts
@@ -1,20 +1,11 @@
 import {MinimalProxy} from 'be-decorated/types';
 
 export interface BeSidelinedVEndUserProps {
-    set: string,
-    onClosest: string,
-    toVal: any,
-    when: string,
-    is: string,
-    outsideClosest: string,
-    onEventType: string
+
 }
 
 export interface BeSidelinedVirtualProps extends BeSidelinedVEndUserProps, MinimalProxy{
-    valsDoNotMatch: boolean;
-    valsMatch: boolean;
-    propChangeCnt: number;
-    closestRef: WeakRef<Element> | undefined;
+
 }
 
 export interface BeSidelinedProps extends BeSidelinedVirtualProps{
@@ -22,11 +13,6 @@ export interface BeSidelinedProps extends BeSidelinedVirtualProps{
 }
 
 export interface BeSidelinedActions{
-    subscribeToProp(self: this): Promise<void>;
-    compareVals(self: this): void;
-    addOutsideListener(self: this): void;
-    removeOutsideListener(self: this): void;
-    addLocalListener(self: this): void;
-    finale(proxy: Element & BeSidelinedVirtualProps, target:Element): Promise<void>; 
+ 
 }
 
